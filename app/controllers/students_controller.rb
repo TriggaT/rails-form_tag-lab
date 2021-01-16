c2c850df88de74794820eaa60cf83de4823a5d5c
Trayshawn <traytwebb14@yahoo.com>
2021-01-16 00:45:39 -0600

    commit tree efd5d2c7c7055235ffb96f420d852b835a07ade5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+
+
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +9,13 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+  end 
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end 
 
 end
